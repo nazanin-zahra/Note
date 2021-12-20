85a43c59741a37a912330bec2da2ef4803476238
Nazanin Zahra <n.beigirad@gmail.com>
2021-12-20 18:46:42 +0330

    improve ui of NoteItems

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,33 +1,97 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-import androidx.compose.desktop.DesktopMaterialTheme
-import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
-@Composable
-@Preview
-fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "Note"
+    ) {
+        MaterialTheme {
+            Row {
+                val notes = remember {
+                    mutableStateListOf(
+                        Note("today was the best ."),
+                        Note("i was unhappy todayyyy  cause i couldnt end up my exams with good gradddddesss."),
+                        Note("you have english class today .i have a lot of things to do . "),
+                        Note("am i crazy?"),
+                    )
+                }
+                LazyColumn(
+                    modifier = Modifier
+                        .weight(weight = 0.4f)
+                        .background(color = Color.LightGray)
+                        .fillMaxHeight()
+                ) {
+                    items(notes) { note ->
+                        NoteItem(
+                            data = note.data,
+                            deleteNote = {
+                                notes.remove(note)
+                            }
 
-    MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+
+                        )
+                    }
+                }
+                Column(
+                    modifier = Modifier
+                        .weight(0.6f)
+                        .fillMaxHeight(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+
+                }
+            }
         }
     }
 }
 
-fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
-        App()
+@Composable
+fun NoteItem(data: String, deleteNote: () -> Unit) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .clip(RoundedCornerShape(8.dp))
+            .clickable {  }
+            .padding(8.dp)
+            .fillMaxWidth()
+         //   .background(color = Color.LightGray)
+
+
+    ) {
+        Spacer(modifier = Modifier.height(12.dp))
+        Text(
+            text = data,
+            color = Color.Black,
+            fontWeight = FontWeight.Bold,
+            maxLines =1,
+            modifier = Modifier.weight(1f)
+        )
+        IconButton(onClick = {
+            deleteNote()
+        }) {
+            Icon(
+                imageVector = Icons.Default.Delete,
+                contentDescription = null
+            )
+        }
+
     }
 }
