ead041a050cad6933798cf4092178fc46b17d9cf
nazi <n.beigirad@gmail.com>
2021-12-24 10:52:55 +0330

    add error for empty notes . color the selected items.add plus icon.add place holder.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,12 +3,16 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -30,6 +34,7 @@ fun main() = application {
             Row {
                 val clickedIndex = remember { mutableStateOf(0) }
                 val noteState = remember { mutableStateOf("") }
+                val noteErrorState = remember { mutableStateOf(false) }
                 val notes = remember {
                     mutableStateListOf(
                         Note("today was the best ."),
@@ -44,7 +49,8 @@ fun main() = application {
                         .background(color = Color.LightGray)
                         .fillMaxHeight()
                 ) {
-                    items(notes) { note ->
+                    itemsIndexed(notes) { index, note ->
+                        val thisNoteIsSelected: Boolean = clickedIndex.value == index
                         NoteItem(
                             data = note.data,
                             deleteNote = {
@@ -53,42 +59,82 @@ fun main() = application {
                             onNoteClicked = {
                                 noteState.value = note.data
                                 clickedIndex.value = notes.indexOf(note)
-                            }
-
+                                noteErrorState.value = false
+                            },
+                            isSelected = thisNoteIsSelected
 
                         )
                     }
                 }
+
                 Box(
-                    modifier = Modifier
+                    modifier = Modifier.fillMaxHeight()
                         .weight(0.6f)
-                        .fillMaxHeight()
                 ) {
-                    Column (
-                        horizontalAlignment = Alignment.CenterHorizontally
-                            ){
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier.align(Alignment.TopCenter)
+                            .fillMaxWidth()
+                    ) {
                         OutlinedTextField(
                             value = noteState.value,
                             onValueChange = {
                                 noteState.value = it
                             },
                             label = {
-                                Text("note")
+                                Text("Note")
+                            },
+                            placeholder = {
+                                Text("Enter note...")
                             },
                             colors = TextFieldDefaults.outlinedTextFieldColors(),
                             modifier = Modifier
                                 .padding(8.dp)
-
+                                .fillMaxWidth()
+                                .fillMaxHeight(0.7f),
+                            isError = noteErrorState.value
                         )
+                        if (noteErrorState.value) {
+                            Text(
+                                text = "Note is empty !",
+                                color = MaterialTheme.colors.error,
+                                style = MaterialTheme.typography.caption,
+                                modifier = Modifier
+                                    .padding(horizontal = 8.dp)
+                                    .align(alignment = Alignment.Start)
+                            )
+                        }
 
                         Button(onClick = {
-                            val clickedNote = notes[clickedIndex.value]
-                            val newNote = clickedNote.copy(data = noteState.value)
-                            notes[clickedIndex.value] = newNote
+                            if (noteState.value == "") {
+                                noteErrorState.value = true
+                            } else {
+                                val clickedNote = notes[clickedIndex.value]
+                                val newNote = clickedNote.copy(data = noteState.value)
+                                notes[clickedIndex.value] = newNote
+                            }
                         }
                         ) {
-                            Text("save")
+                            Text("Save")
                         }
+                    }
+
+
+                    FloatingActionButton(
+                        onClick = {
+                            noteState.value = ""
+                            val element = Note(data = "")
+                            notes.add(element)
+                            clickedIndex.value = notes.indexOf(element)
+                            noteErrorState.value = false
+                        },
+                        modifier = Modifier
+                            .align(alignment = Alignment.BottomEnd)
+                            .padding(end = 16.dp, bottom = 16.dp),
+                        backgroundColor = Color(0xffff0078),
+                        contentColor = Color.White
+                    ) {
+                        Icon(imageVector = Icons.Default.Add, contentDescription = null)
                     }
                 }
             }
@@ -97,17 +143,22 @@ fun main() = application {
 }
 
 @Composable
-fun NoteItem(data: String, deleteNote: () -> Unit, onNoteClicked: () -> Unit) {
+fun NoteItem(data: String, deleteNote: () -> Unit, onNoteClicked: () -> Unit, isSelected: Boolean) {
     Row(
         verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier
-            .clip(RoundedCornerShape(8.dp))
-            .clickable { onNoteClicked() }
-            .padding(8.dp)
-            .fillMaxWidth()
-        //   .background(color = Color.LightGray)
-
-
+        modifier = if (isSelected)
+            Modifier
+                .clip(RoundedCornerShape(8.dp))
+                .background(color = Color.Gray)
+                .clickable { onNoteClicked() }
+                .padding(8.dp)
+                .fillMaxWidth()
+        else
+            Modifier
+                .clip(RoundedCornerShape(8.dp))
+                .clickable { onNoteClicked() }
+                .padding(8.dp)
+                .fillMaxWidth()
     ) {
         Spacer(modifier = Modifier.height(12.dp))
         Text(
